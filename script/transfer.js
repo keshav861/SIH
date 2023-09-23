440f718ebb5149b541ebbbb6ca1e9454f24ef512
Harshit Gupta <114746121+Harshit-gupta-45107@users.noreply.github.com>
2023-09-24 04:52:41 +0530

    Update transfer.js

--- script/transfer.js
+++ script/transfer.js
@@ -1,8 +1,8 @@
 let x, y;
 var emailsClient = ["client1@gmail.com", "client3@gmail.com", "client2@gmail.com"];
 var emailsConsultant = ["consultant1@gmail.com", "consultant3@gmail.com", "consultant2@gmail.com"];
-document.querySelectorAll("input")[4].addEventListener("click", function(event) {
-
+document.querySelectorAll("input")[3].addEventListener("keypress", function(event) {
+        if(event.key=="Enter")
         x = (document.forms["form"]["email"].value);
     
     if (emailsClient.includes(x))
