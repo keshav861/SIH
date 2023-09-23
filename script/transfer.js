24cdda8d2c67b3d3a06d8dc3fc856cc9809541ce
Harshit Gupta <114746121+Harshit-gupta-45107@users.noreply.github.com>
2023-09-24 02:38:55 +0530

    Add files via upload

---
+++ script/transfer.js
@@ -0,0 +1,14 @@
+let x, y;
+var emailsClient = ["client1@gmail.com", "client3@gmail.com", "client2@gmail.com"];
+var emailsConsultant = ["consultant1@gmail.com", "consultant3@gmail.com", "consultant2@gmail.com"];
+document.querySelectorAll("input")[3].addEventListener("keypress", function(event) {
+    if (event.key == "Enter") {
+        x = (document.forms["form"]["email"].value);
+        y = (document.forms["form"]["password"].value);
+    }
+    if (emailsClient.includes(x) && y == "client")
+        document.querySelector("#form").setAttribute("action", "https://www.google.com");
+    else if (emailsConsultant.includes(x) && y == "consultant")
+        document.querySelector("#form").setAttribute("action", "https://www.bing.com");
+    console.log(x);
+})
