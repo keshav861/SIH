8a101fb456f5b17995ac23b0c361550452cf2bf9
Harshit Gupta <114746121+Harshit-gupta-45107@users.noreply.github.com>
2023-09-24 03:43:52 +0530

    Update transfer.js

--- script/transfer.js
+++ script/transfer.js
@@ -6,11 +6,12 @@ document.querySelectorAll("input")[3].addEventListener("keypress", function(even
         x = (document.forms["form"]["email"].value);
         y = (document.forms["form"]["password"].value);
     if (emailsClient.includes(x) && y == "client")
-    {document.querySelector("#form").setAttribute("action", "https://www.google.com");
-     document.querySelector("#form").reset();
+    {document.querySelector("#form").reset();
+        document.querySelector("#form").setAttribute("action", "https://www.google.com");
+     
     }
     else if (emailsConsultant.includes(x) && y == "consultant")
-    {document.querySelector("#form").setAttribute("action", "https://www.bing.com");
-     document.querySelector("#form").reset();
+    {document.querySelector("#form").reset();
+        document.querySelector("#form").setAttribute("action", "https://www.bing.com");
     }
 }})
