5d6efe8afd7afa8ae0a03c9bae70cfa10def4d76
Keshav Maheshwari <94895949+keshav861@users.noreply.github.com>
2023-09-24 08:43:35 +0530

    Add files via upload

--- script/transfer.js
+++ script/transfer.js
@@ -7,9 +7,9 @@ document.querySelectorAll("input")[3].addEventListener("keypress", function(even
         y = (document.forms["form"]["password"].value);
     }
     if (emailsClient.includes(x) && y == "user")
-        document.querySelector("#form").setAttribute("action", "/user_interface/index2.html");
+        document.querySelector("#form").setAttribute("action", "../user_interface/index2.html");
     else if (emailsConsultant.includes(x) && y == "consultant")
-        document.querySelector("#form").setAttribute("action", "https://www.bing.com");
+        document.querySelector("#form").setAttribute("action", "#");
 
     console.log(x);
 })
