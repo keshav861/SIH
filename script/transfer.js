b3ed13da2510563d76da62dd6ca4ccbe45f8f5c5
Harshit Gupta <114746121+Harshit-gupta-45107@users.noreply.github.com>
2023-09-24 07:16:01 +0530

    Update transfer.js

--- script/transfer.js
+++ script/transfer.js
@@ -1,18 +1,15 @@
 let x, y;
 var emailsClient = ["client1@gmail.com", "client3@gmail.com", "client2@gmail.com"];
 var emailsConsultant = ["consultant1@gmail.com", "consultant3@gmail.com", "consultant2@gmail.com"];
-console.log(document.querySelectorAll("input")[3]);
 document.querySelectorAll("input")[3].addEventListener("keypress", function(event) {
     if (event.key == "Enter") {
         x = (document.forms["form"]["email"].value);
         y = (document.forms["form"]["password"].value);
     }
-    if (emailsClient.includes(x) && y == "client") {
+    if (emailsClient.includes(x) && y == "user")
         document.querySelector("#form").setAttribute("action", "https://www.google.com");
-        document.forms["form"]["password"].value = "";
-    } else if (emailsConsultant.includes(x) && y == "consultant") {
+    else if (emailsConsultant.includes(x) && y == "consultant")
         document.querySelector("#form").setAttribute("action", "https://www.bing.com");
-        document.forms["form"]["password"].value = "";
-    }
+
     console.log(x);
 })
