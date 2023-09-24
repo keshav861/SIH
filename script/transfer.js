e3f9c9e3f5fbffa27e1c816e9392b6b62c8a3989
Harshit Gupta <114746121+Harshit-gupta-45107@users.noreply.github.com>
2023-09-24 22:42:41 +0530

    Update transfer.js

--- script/transfer.js
+++ script/transfer.js
@@ -1,45 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
-
-// Middleware to parse JSON data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Simulated user data (replace with a database)
-const users = [
-    { id: 1, email: 'user@example.com', password: 'password123' },
-    // Add more users as needed
-];
-
-// Serve static files (HTML, CSS, JavaScript)
-app.use(express.static(__dirname));
-
-// Route for handling login POST requests
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-
-    // Simulated user authentication
-    const user = users.find((user) => user.email === email && user.password === password);
-
-    if (user) {
-        res.send('Login successful');
-    } else {
-        res.status(401).send('Login failed');
+let x, y;
+var emailsClient = ["client1@gmail.com", "client3@gmail.com", "client2@gmail.com"];
+var emailsConsultant = ["consultant1@gmail.com", "consultant3@gmail.com", "consultant2@gmail.com"];
+document.querySelectorAll("input")[3].addEventListener("keypress", function(event) {
+    if (event.key == "Enter") {
+        x = (document.forms["form"]["email"].value);
+        y = (document.forms["form"]["password"].value);
     }
-});
+    if (emailsClient.includes(x) && y == "user")
+        document.querySelector("#form").setAttribute("action", "https://www.google.com");
+    else if (emailsConsultant.includes(x) && y == "consultant")
+        document.querySelector("#form").setAttribute("action", "index2.html");
 
-// Route for handling signup POST requests
-app.post('/signup', (req, res) => {
-    const { email, password } = req.body;
-
-    // Simulated user registration (add to the users array)
-    users.push({ id: users.length + 1, email, password });
-
-    res.send('Signup successful');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+    console.log(x);
+})
