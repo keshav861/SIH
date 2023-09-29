745cb4a4842a1d24e4fb47c8ef2903398eee7c5b
Harshit Gupta <114746121+Harshit-gupta-45107@users.noreply.github.com>
2023-09-29 13:46:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ const port = 5500;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-})
+// app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/index.html")
+// })
 
-app.get("/pages/login.html", (req, res) => {
-    res.sendFile(__dirname + "/pages/login.html");
-})
+// app.get("/pages/login.html", (req, res) => {
+//     res.sendFile(__dirname + "/pages/login.html");
+// })
 
 app.post("/submit", (req, res) => {
     if (req.body["email"] == "client1@gmail.com" && req.body["password"] == "user")
         res.send("<h1>Clinet login</h1>");
     else if (req.body["email"] == "consultant1@gmail.com" && req.body["password"] == "user")
-        res.sendFile(__dirname + "/user_interface/index2.html");
+        res.sendFile("../user_interface/index2.html");
     else
-        res.sendFile(__dirname + "/pages/login.html");
+        res.sendFile("./pages/login.html");
 })
 
 app.listen(port, () => {
