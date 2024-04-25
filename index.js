d9810ce888dd4da65a195efff0172761896b8d69
Harshit Gupta <guptaharshit7718@gmail.com>
2024-04-25 18:20:04 +0530

    Signup/login

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 import express from "express";
 // import serverless from 'serverless-http;
 import bodyParser from "body-parser";
@@ -16,43 +16,63 @@ mongoose.connect("mongodb://127.0.0.1:27017/legal-easy", { useNewUrlParser: true
 const personSchema = new mongoose.Schema({
     name: String,
     password: String,
-    email: String,
-    number: Number,
-    // username: String
+    email: {
+        type: String,
+        unique: true,
+      },
+    number: {
+        type: Number,
+        unique: true,
+        required: true,
+      },
 });
 
 const serverSchema = new mongoose.Schema({
     name: String,
     password: String,
-    email: String,
-    brnumber: String,
-    username: String
+    email: {
+        type: String,
+        unique: true,
+      },
+    brnumber: {
+        type: String,
+        unique: true,
+        required: true,
+      },
+      number: {
+        type: Number,
+        unique: true,
+        required: true,
+      },
 });
 
 const Person = mongoose.model("Person", personSchema);
 
 const Server = mongoose.model("Server", serverSchema);
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
+
 app.post("/login", async(req, res) => {
-    const person = await Person.findOne({ email: req.body["email"] }).exec();
-    const server = await Server.findOne({ email: req.body["email"] }).exec();
+    const person = await Person.findOne({ number: req.body["number"] }).exec();
+    const server = await Server.findOne({ number: req.body["number"] }).exec();
 
     if (person) {
-        if (req.body["password"] == person.password)
+        if (req.body["password"] === person.password)
             res.sendFile(__dirname + "/client_interface/index2.html");
         else
             res.sendFile(__dirname + "/pages/login.html");
     } else if (server) {
-        if (req.body["password"] == server.password)
+        if (req.body["password"] === server.password)
             res.sendFile(__dirname + "/user_interface/index2.html");
         else
             res.sendFile(__dirname + "/pages/login.html");
     } else
         res.sendFile(__dirname + "/pages/signup.html");
 })
+
 
 app.post("/signup", async(req, res) => {
     const existing = await Person.findOne({ number: req.body["number"] }).exec();
@@ -63,17 +83,20 @@ app.post("/signup", async(req, res) => {
             name: req.body["name"],
             password: req.body["password"],
             email: req.body["email"],
-            username: req.body["username"],
             number: req.body["number"]
         }).then((data) => {});
-        if (req.body["password"] == req.body["conf_password"]) {
+        if (req.body["password"] === req.body["conf_password"]) {
             res.sendFile(__dirname + "/client_interface/index2.html");
+        }
+        else{
+            res.send("Passwords Don't Match");
         }
     }
 })
 
-app.post("/server", async(req, res) => {
-    const existing = await Server.findOne({ brnumber: req.body["brnumber"] }).exec();
+
+app.post("/server-signup", async(req, res) => {
+    const existing = await Server.findOne({ number: req.body["number"] }).exec();
     if (existing) {
         res.send("Service Provider Exists");
     } else {
@@ -81,7 +104,7 @@ app.post("/server", async(req, res) => {
             name: req.body["name"],
             password: req.body["password"],
             email: req.body["email"],
-            username: req.body["username"],
+            number: req.body["number"],
             brnumber: req.body["brnumber"]
         }).then((data) => {});
         if (req.body["password"] == req.body["conf_password"]) {
@@ -89,6 +112,7 @@ app.post("/server", async(req, res) => {
         }
     }
 })
+
 
 app.listen(port, () => {
     console.log(`${port}`);
