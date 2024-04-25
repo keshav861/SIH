9b6bd65f22b1888bd719b10b8f0e1abf7b9e6b05
Keshav Maheshwari <nawallkeshav@gmail.com>
2024-04-25 17:26:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const __dirname = dirname(fileURLToPath(
     import.meta.url));
 
 const app = express();
-const port = process.env.PORT;
-// const port = 5500;
+const port = process.env.PORT || 5500;
 
 //change it when runjning on git
 
